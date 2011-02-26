e6a5dce7726c299c57b02f7cc2cecbd592e287af
Ryan J <ryanjayako@gmail.com>
2011-02-26 18:06:20 -0500

    First Deploy

---
+++ jtextedit/FileModel.java
@@ -0,0 +1,22 @@
+// Copyright (c) 2011
+//===== Description: ========================================= 
+//
+//===== Additional Comments: ================================= 
+//
+//============================================================ 
+
+/**
+ * 
+ */
+package jtextedit;
+
+
+public class FileModel {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
