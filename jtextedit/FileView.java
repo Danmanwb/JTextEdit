b53cbad4dd138f6e86d85ff19c9ab952fb79e90d
Ryan J <ryanjayako@gmail.com>
2011-02-26 18:26:02 -0500

    Added FileController and FileView and a .gitignore file

---
+++ jtextedit/FileView.java
@@ -0,0 +1,13 @@
+// Copyright (c) 2011, Ryan J ryanjayako@gmail.com 
+//===== Description: ========================================= 
+//
+//===== Additional Comments: ================================= 
+//
+//============================================================ 
+
+package jtextedit;
+
+public class FileView {
+
+
+}
